ec8ec5cd6b9c4f2011e7ef0362900dd13daf3b4e
Paul Sinnett <paul.sinnett@gmail.com>
2018-12-04 01:26:16 +0000

    initial

---
+++ Assets/LaunchProjectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour
+{
+	public Rigidbody projectilePrefab;
+	public float angle;
+	public float force;
+
+	void OnEnable()
+	{
+		Rigidbody projectile = Instantiate(projectilePrefab, transform);
+		Quaternion elevation = Quaternion.Euler(0.0f, 0.0f, angle);
+		projectile.AddForce(elevation * Vector3.right * force, ForceMode.Impulse);
+		enabled = false;
+	}
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	Rigidbody physics;
+
+	void Awake()
+	{
+		physics = GetComponent<Rigidbody>();
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		physics.AddForce(-physics.velocity, ForceMode.VelocityChange);
+	}
+}
